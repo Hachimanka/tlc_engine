ee3785420f48ac6f7d939e247a7c599477db0888
goofygoober <villamilrhona@gmail.com>
2026-04-15 00:19:34 +0800

    Notifications
    -added basic notification box

--- components/tenant-page/HeaderTenant.tsx
+++ components/tenant-page/HeaderTenant.tsx
@@ -3,19 +3,50 @@
 import LogoContainer from "./LogoContainer";
 import NotificationBadge from "./NotificationBadge";
 import ProfileBadge from "./ProfileBadge";
+import { useState } from "react";
 
 function HeaderTenant() {
+  const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+
   return (
-    <nav className="h-[72px] bg-[var(--color-primary)]">
-      <div className="flex h-full w-full items-center justify-between px-4 md:px-6">
-        <LogoContainer />
-        <div className="flex items-center gap-4">
-          <NotificationBadge />
-          <div className="w-px h-10 bg-white/30" />
-          <ProfileBadge />
+    <div>
+      <nav className="h-[72px] bg-[var(--color-primary)]">
+        <div className="flex h-full w-full items-center justify-between px-4 md:px-6">
+          <LogoContainer />
+          <div className="flex items-center gap-4">
+            <NotificationBadge onTrigger={() => setIsNotificationOpen(true)} />
+            {isNotificationOpen && (
+              <div className="absolute right-13 mt-45 w-60 bg-[var(--color-card)] border-1 border-[var(--color-primary)] rounded-lg shadow-md z-50 overflow-hidden">
+                <div className="p-3 border-b font-semibold text-header-large mb-4">
+                  Notifications
+                  <button
+                    onClick={() => setIsNotificationOpen(false)}
+                    className="ml-25 font-bold cursor-pointer"
+                  >
+                    ✕
+                  </button>
+                </div>
+
+                <div className="max-h-64 overflow-y-auto">
+                  {/* <div className="p-3 text-sm hover:bg-gray-50 border-b last:border-0">
+              Notification 1: You have a new message.
+            </div>
+            <div className="p-3 text-sm hover:bg-gray-50 border-b last:border-0">
+              Server update successful!
+            </div> */}
+                  <div className="p-3 text-sm text-gray-500 mb-5">
+                    No notifications
+                  </div>
+                </div>
+              </div>
+            )}
+
+            <div className="w-px h-10 bg-white/30" />
+            <ProfileBadge />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
 
--- components/tenant-page/NotificationBadge.tsx
+++ components/tenant-page/NotificationBadge.tsx
@@ -1,15 +1,22 @@
+"use client";
 import { AppIcon } from "@/public/icons";
+import { useState, useEffect, useRef } from "react";
 
-export const NotificationBadge = () => {
+export default function NotifBox({ onTrigger }: { onTrigger: () => void }) {
+  // const showNotification = () => {
+  //   setIsNotificationOpen(true);
+  //   setTimeout(() => setIsNotificationOpen(false), 3000);
+  // };
+
   return (
     <div className="">
-      <AppIcon
-        name="bell"
-        className="rounded-full p-2 flex items-center justify-center bg-[#C5EEEA] [&_svg_path]:stroke-[var(--color-primary)]  w-8 h-8 "
-        title="Bell Icon"
-      />
+      <button onClick={onTrigger}>
+        <AppIcon
+          name="bell"
+          className="rounded-full p-2 flex items-center justify-center bg-[#C5EEEA] [&_svg_path]:stroke-[var(--color-primary)]  w-8 h-8 cursor-pointer hover:bg-[#C5EEEA]/80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[var(--color-primary)]"
+          title="Bell Icon"
+        />
+      </button>
     </div>
   );
-};
-
-export default NotificationBadge;
+}
