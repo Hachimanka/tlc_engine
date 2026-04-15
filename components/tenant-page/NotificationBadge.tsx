d8bb679e56bd58576096daa9b87969d259b25023
goofygoober <villamilrhona@gmail.com>
2026-04-16 01:14:35 +0800

    Notifications Adjustments and Profile Animations
    -changed the x icon in the notification dropdown box
    -added scale and fade-in animations in the profile badge and dropdown box

--- components/tenant-page/NotificationBadge.tsx
+++ components/tenant-page/NotificationBadge.tsx
@@ -9,7 +9,7 @@ interface NotifBoxProps {
 export default function NotifBox({ onTrigger, active }: NotifBoxProps) {
   return (
     <div className="">
-      <button
+      <div
         onClick={onTrigger}
         // Use active state to trigger a scale effect
         className={`transition-transform duration-200 ${active ? "scale-90" : "scale-100"}`}
@@ -19,7 +19,7 @@ export default function NotifBox({ onTrigger, active }: NotifBoxProps) {
           className="rounded-full p-2 flex items-center justify-center bg-[#C5EEEA] [&_svg_path]:stroke-[var(--color-primary)] w-8 h-8 cursor-pointer hover:bg-[#C5EEEA]/80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[var(--color-primary)]"
           title="Bell Icon"
         />
-      </button>
+      </div>
     </div>
   );
 }
--- components/tenant-page/ProfileBadge.tsx
+++ components/tenant-page/ProfileBadge.tsx
@@ -1,17 +1,29 @@
 import { AppIcon } from "@/public/icons";
-import { NotificationBadge } from "./NotificationBadge";
 
-export const ProfileBadge = () => {
+interface ProfileBadgeProps {
+  onTtrigger: () => void;
+  active: boolean; // Add this prop
+}
+
+export default function ProfileBadge({
+  onTtrigger,
+  active,
+}: ProfileBadgeProps) {
   return (
-    <div className="rounded-full border border-white/15 bg-white  px-1 py-1 flex items-center justify-start text-body-medium text-[var(--high-emphasis)] ">
-      <AppIcon
-        name="people"
-        className="rounded-full p-2 flex items-center justify-center [&_svg_path]:stroke-[var(--color-primary)] bg-[#C5EEEA] text-white w-8 h-8"
-        title="People Icon"
-      />
-      <div className="px-3 mr-2">Leonard Forrosuelo</div>
+    <div
+      onClick={onTtrigger}
+      className={`transition-transform duration-200 ${active ? "scale-90" : "scale-95"}`}
+    >
+      <div className="rounded-full border border-white/15 bg-white  px-1 py-1 flex items-center justify-start text-body-medium text-[var(--high-emphasis)] cursor-pointer hover:bg-white/90 transition-colors duration-200">
+        <div>
+          <AppIcon
+            name="people"
+            className="rounded-full p-2 flex items-center justify-center [&_svg_path]:stroke-[var(--color-primary)] bg-[#C5EEEA] text-white w-8 h-8"
+            title="People Icon"
+          />
+        </div>
+        <div className="px-3 mr-2">Leonard Forrosuelo</div>
+      </div>
     </div>
   );
-};
-
-export default ProfileBadge;
+}
