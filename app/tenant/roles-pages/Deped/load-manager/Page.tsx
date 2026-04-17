b5c00a374af72a680ae9a460df02848db09be7a1
John Michael M. Inoc <johnmichael.inoc@cit.edu>
2026-04-17 14:30:20 +0800

    feat: implement faculty management features with AddFacultyForms and DepartmentFacultyTable components

--- app/tenant/roles-pages/Deped/load-manager/Page.tsx
+++ app/tenant/roles-pages/Deped/load-manager/Page.tsx
@@ -1,7 +1,14 @@
+import DepartmentFacultyTable from "@/components/roles/Deped/load-manager/components/DepartmentFacultyTable";
+
 export default function TenantPage() {
   return (
-    <main>
-      <h1>Hello Welcome to LOAD MANAGER World!</h1>
+    <main className="min-h-screen bg-[var(--color-background)] px-4 py-4 sm:px-6 lg:px-8">
+      <div className="mx-auto w-full max-w-[1400px] space-y-2 font-ibm-plex-sans">
+        <h1 className="text-heading-h3 text-[var(--color-primary)]">
+          Teaching Load Assignment
+        </h1>
+        <DepartmentFacultyTable />
+      </div>
     </main>
-  )
+  );
 }
